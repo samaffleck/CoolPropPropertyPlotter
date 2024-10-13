e1b3fd5407256529b4c560a1b72c2ec198209c5c
sam Affleck <sam.affleck@hotmail.com>
2024-10-13 15:52:02 +0100

    Removed unused imports

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,9 +5,7 @@ export const metadata = {
 
 import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
-import Workflows from "@/components/workflows";
 import Features from "@/components/features";
-import Testimonials from "@/components/testimonials";
 import Cta from "@/components/cta";
 
 export default function Home() {
